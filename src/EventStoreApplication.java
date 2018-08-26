bbc194224b354d33e82ca4b4097e397567ab2e71
Francinei Gomes de Morais <fgomesm@yahoo.com.br>
2018-08-25 21:12:36 -0300

    Update EventStoreApplication.java

--- src/EventStoreApplication.java
+++ src/EventStoreApplication.java
@@ -10,7 +10,7 @@ public class EventStoreApplication {
 	    public static void main(String[] args) {
 
 	    	// Memory allocation
-			EventStore eventStoreService = new EventStoreService();
+		EventStore eventStoreService = new EventStoreService();
 
 	    	// Creating events
 	    	Event event_01 = new Event("type_01", 123L);
@@ -23,48 +23,48 @@ public class EventStoreApplication {
 	    	Event event_08 = new Event("type_05", 823L);
 	    	Event event_09 = new Event("type_05", 923L);
 
-			// Insert event
-			eventStoreService.insert(event_01);
-			eventStoreService.insert(event_02);
-			eventStoreService.insert(event_03);
-			eventStoreService.insert(event_04);
-			eventStoreService.insert(event_05);
-			eventStoreService.insert(event_06);
-			eventStoreService.insert(event_07);
-			eventStoreService.insert(event_08);
-			eventStoreService.insert(event_09);
+		// Insert event
+		eventStoreService.insert(event_01);
+		eventStoreService.insert(event_02);
+		eventStoreService.insert(event_03);
+		eventStoreService.insert(event_04);
+		eventStoreService.insert(event_05);
+		eventStoreService.insert(event_06);
+		eventStoreService.insert(event_07);
+		eventStoreService.insert(event_08);
+		eventStoreService.insert(event_09);
 
-			// Removing event
-			eventStoreService.removeAll("type_02");
+		// Removing event
+		eventStoreService.removeAll("type_02");
 
-			// Get subList
-			EventIterator iterSubList = eventStoreService.query("type_05", 623L, 923L);
+		// Get subList
+		EventIterator iterSubList = eventStoreService.query("type_05", 623L, 923L);
 
-			try {
-				iterSubList.moveNext();
-				//iterSubList.moveNext();
-				//iterSubList.moveNext();
-				//iterSubList.remove();
-				//iterSubList.remove();
-			}
-			catch (IllegalStateException ex) {
-				System.out.println(ex.getMessage());
-			}				
+		try {
+			iterSubList.moveNext();
+			//iterSubList.moveNext();
+			//iterSubList.moveNext();
+			//iterSubList.remove();
+			//iterSubList.remove();
+		}
+		catch (IllegalStateException ex) {
+			System.out.println(ex.getMessage());
+		}				
 
-			try {				
-				iterSubList.moveNext();
-				Event currentEvent = iterSubList.current();
-				System.out.println("Current event : " + currentEvent.type() + ", timestamp: "+ currentEvent.timestamp()+ " to subList");
-				
-				currentEvent = iterSubList.current();
-				System.out.println("Current event : " + currentEvent.type() + ", timestamp: "+ currentEvent.timestamp()+ " to subList");
-			
-			}
-			catch (IllegalStateException ex) {
-				System.out.println(ex.getMessage());
-			}
-			catch (NoSuchElementException ex) {
-				System.out.println("The iterador reached the end of Sublist");
-			}					
+		try {				
+			iterSubList.moveNext();
+			Event currentEvent = iterSubList.current();
+			System.out.println("Current event : " + currentEvent.type() + ", timestamp: "+ currentEvent.timestamp()+ " to subList");
+
+			currentEvent = iterSubList.current();
+			System.out.println("Current event : " + currentEvent.type() + ", timestamp: "+ currentEvent.timestamp()+ " to subList");
+
+		}
+		catch (IllegalStateException ex) {
+			System.out.println(ex.getMessage());
+		}
+		catch (NoSuchElementException ex) {
+			System.out.println("The iterador reached the end of Sublist");
+		}					
 	    }
 }
