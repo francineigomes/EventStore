4536b135877299dd21e6b8b0a4d42cc17c3d04a4
Francinei Gomes de Morais <fgomesm@yahoo.com.br>
2018-08-26 13:17:50 -0300

    Update EventStoreServiceTest.java

--- src/EventStoreServiceTest.java
+++ src/EventStoreServiceTest.java
@@ -1,8 +1,6 @@
 package net.intelie.challenges;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.jupiter.api.Assertions.*;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.ListIterator;
@@ -15,24 +13,24 @@ class EventStoreServiceTest {
 	@Test
 	void testInsert() {
 		//fail("Not yet implemented");
-    	// Memory allocation
+		// Memory allocation
 		EventStoreService eventStoreService = new EventStoreService();
 		// Creating events
 		Event event_01 = new Event("type_01", 123L);
 		Event event_02 = new Event("type_02", 223L);
-		
-        Logger l = Logger.getLogger(getClass().getName());
-        l.info("Type: " + event_01.type());
-        l.info("Type: " + event_02.type());
-        
+
+		Logger l = Logger.getLogger(getClass().getName());
+		l.info("Type: " + event_01.type());
+		l.info("Type: " + event_02.type());
+
 		eventStoreService.insert(event_01);
 		eventStoreService.insert(event_02);
-		
+
 		List<Event> events = new ArrayList<Event>();
 		events.addAll(eventStoreService.getEvents());
-		
+
 		ListIterator<Event> iter = events.listIterator();
-		
+
 		assertEquals("type_01",iter.next().type());
 		assertEquals("type_02",iter.next().type());
     	
